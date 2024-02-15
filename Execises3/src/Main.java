7442a12cc6d39c13db114002f5cd04996a8dbf8a
lamia49 <80590112+lamia49@users.noreply.github.com>
2024-02-15 18:08:49 +0300

    Add files via upload

---
+++ Execises3/src/Main.java
@@ -0,0 +1,129 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Qustion1
+
+
+//      ArrayList<Integer> numbers=new ArrayList<Integer>();
+//        numbers.add(50);
+//        numbers.add(-20);
+//        numbers.add(0);
+//        numbers.add(30);
+//        numbers.add(40);
+//        numbers.add(60);
+//        numbers.add(10);
+//        boolean chek=numbers.get(0)== numbers.get(6);
+//        System.out.println(numbers);
+//        if(chek){
+//            System.out.print(chek);
+//        }else {
+//            System.out.print(chek);
+//        }
+
+
+        //Qstion2
+
+
+//        ArrayList<Integer> numbers = new ArrayList<Integer>();
+//        numbers.add(1);
+//        numbers.add(4);
+//        numbers.add(17);
+//        numbers.add(7);
+//        numbers.add(25);
+//        numbers.add(3);
+//        numbers.add(100);
+//        int count = 0;
+//        ArrayList<Integer> great=new ArrayList<Integer>();
+//        for (int i = 0; i < numbers.size(); i++) {
+//            count += numbers.get(i);
+//        }
+//        int ava = count / 7;
+//        for (int j : numbers) {
+//            if (j > ava) {
+//                great.add(j);
+//            }
+//        }
+//        System.out.print("the avrage is: " + ava + " the number is greater then avarge is:"+great);
+
+
+        //Qustion3
+
+
+//        ArrayList<Integer>numbers=new ArrayList<Integer>();
+//        numbers.add(20);
+//        numbers.add(30);
+//        numbers.add(40);
+//        if(numbers.get(0)>numbers.get(2)){
+//            System.out.print("the largest number is "+numbers.get(0));
+//        }else {
+//            System.out.print("the largest number is "+numbers.get(2));
+//        }
+
+
+        //qustion4
+
+
+//        ArrayList<Integer> numbers=new ArrayList<Integer>();
+//        numbers.add(20);
+//        numbers.add(30);
+//        numbers.add(40);
+//        ArrayList<Integer>afterswap=new ArrayList<Integer>();
+//            afterswap.add(numbers.get(2));
+//            afterswap.add(numbers.get(1));
+//            afterswap.add(numbers.get(0) );
+//         System.out.print(afterswap);
+
+
+        //quastion5
+
+//        ArrayList<Integer> numbers = new ArrayList<>();
+//        numbers.add(2);
+//        numbers.add(3);
+//        numbers.add(40);
+//        numbers.add(1);
+//        numbers.add(5);
+//        numbers.add(9);
+//        numbers.add(4);
+//        numbers.add(10);
+//        numbers.add(7);
+//        ArrayList<Integer> s = new ArrayList<>();
+//
+//        int count = 0;
+//        int c = 0;
+//        for (int n : numbers) {
+//            if (n % 2 != 0) {
+//                s.add(n);
+//            }
+//        }
+//        for (int n : numbers) {
+//            if (n % 2 == 0) {
+//                s.add(n);
+//            }
+//        } System.out.println(s);
+
+
+        //Quastion6
+        
+
+//        ArrayList<Integer> s1 = new ArrayList<>();
+//        s1.add(2);
+//        s1.add(3);
+//        s1.add(6);
+//        s1.add(6);
+//        s1.add(4);
+//        ArrayList<Integer> s2 = new ArrayList<>();
+//        s2.add(2);
+//        s2.add(3);
+//        s2.add(6);
+//        s2.add(6);
+//        s2.add(4);
+//        System.out.print(s1.equals(s2));
+
+
+
+
+    }
+}
